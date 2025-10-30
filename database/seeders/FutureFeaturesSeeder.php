a769fe9986fe09c3f521036fc8551f01c6e71b7a
Hasibulislam8381 <hasibulislam8381@gmail.com>
2025-10-30 17:54:11 +0600

    ddd

--- database/seeders/FutureFeaturesSeeder.php
+++ database/seeders/FutureFeaturesSeeder.php
@@ -12,8 +12,7 @@ class FutureFeaturesSeeder extends Seeder
         FutureFeatures::create([
             'title' => 'Future Features: Core Integration',
             'features' => 'DMS Integration, Secure Data Access, Single Source of Truth, Office Integration',
-            'image' => null, // porer somoy upload korte paro
+            'image' => 'uploads/future_features/17618186491150613299.jpg', // porer somoy upload korte paro
         ]);
-
     }
 }
